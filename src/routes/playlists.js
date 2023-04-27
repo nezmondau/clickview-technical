ab242d442b51f339744ab72fb44f30608c2ff01a
harrywhiteman <nezmondau@outlook.com>
2023-04-27 10:25:49 +0800

    add delete playlist functionality

--- src/routes/playlists.js
+++ src/routes/playlists.js
@@ -6,7 +6,7 @@ import {PlaylistItem} from '../components/playlist-item';
 export function Playlists() {
   const navigate = useNavigate();
   const [isCreateModalOpen, setIsCreateModalOpen] = React.useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = React.useState(false);
+  const [state, setState] = React.useState(false);
 
   const handlePlaylistClick = (id) => {
     navigate(`/playlists/${id}`);
@@ -22,7 +22,16 @@ export function Playlists() {
         dateCreated: new Date(),
       });
       setIsCreateModalOpen(false);
-  }
+  };
+
+  const handleDeletePlaylist = (id) => {
+    playlistData.map((playlist, idx) => {
+      if (playlist.id === id) {
+        playlistData.splice(idx, 1);
+      }
+    });
+    setState(!state);
+  };
 
 
   return (
@@ -30,28 +39,35 @@ export function Playlists() {
       <h1>Playlists route</h1>
       <div>
         <div style={{marginBlock: '10px'}}> 
-
           {isCreateModalOpen ? 
             <div>
               <input type="text" class="form-control" style={{marginBlock: '5px'}} id="playlistName" aria-describedby="playlistName" placeholder="Enter playlist name"></input> 
-              <button type="button" class="btn btn-primary" style={{marginRight: '5px'}} onClick={handleCreatePlaylist}>
+              <button type="button" class="btn btn-primary" onClick={handleCreatePlaylist}>
                 Submit
               </button>
             </div>
             :  
-            <button type="button" class="btn btn-primary" style={{marginRight: '5px'}} onClick={() => setIsCreateModalOpen(true)}>
+            <button type="button" class="btn btn-primary" onClick={() => setIsCreateModalOpen(true)}>
               Create Playlist
             </button>
           }
         </div>
+
         {/* Ensure playlistData has data / falls back if not */}
         {playlistData?.length > 0 
           ? 
             playlistData.map((playlist) => {
               return (
+                <div>
                 <div onClick={() => {handlePlaylistClick(playlist.id)}} style={{cursor: 'pointer'}}>
                   <PlaylistItem key={playlist.id} playlist={playlist} />
                 </div>
+                
+                 <button type="button" class="btn btn-primary" style={{marginBlock: '5px'}}onClick={() => handleDeletePlaylist(playlist.id)}>
+                 Delete Playlist
+               </button>
+               </div>
+               
               )
             }) 
           : 
